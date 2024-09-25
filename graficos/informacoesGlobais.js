a049e9dad3feb15a106700c6e8a975458127c207
sujirokimemame <130391015+sujirokimemame@users.noreply.github.com>
2024-09-25 12:02:43 -0300

    aula 25.09

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,31 @@
+
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+<script type="module" src="graficos/informacoesGlobais.js"></script>
+
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+}
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+        const res = await fetch(url)
+
+    }
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+        const res = await fetch(url)
+
+        const dados = await res.json()
+
+        console.log(dados);
+
+    }
+
+    visualizarInformacoesGlobais()
+    
---
+++ informacoesGlobais.js
@@ -0,0 +1,107 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+console.log(url);
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+// código omitido
+
+async function vizualizarInformacoesGlobais() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+    async function vizualizarInformacoesGlobais() {
+        const res = await fetch(url)
+        const dados = await res.json()
+        console.log(dados);
+    }
+    
+    vizualizarInformacoesGlobais()]
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+async function vizualizarInformacoesGlobais() {
+
+}
+// código omitido
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+}
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados await res.json()
+    console.log(dados.tempo_medio);
+  }
+  
+  vizualizarInformacoesGlobais()
+  async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+  }
+  
+  vizualizarInformacoesGlobais()
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente `
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+  async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+   paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
+  <p class="graficos-container_texto">
+Você sabia que o mundo tem 7888000000 de pessoas e que aproximadamente 5040000000 estão conectadas em alguma rede social e passam em média 2.38 horas conectadas."
+</p>
+// código omitido
+
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+  }
+  
+  vizualizarInformacoesGlobais()
+  async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+  }
+  
+  vizualizarInformacoesGlobais()
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+  async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+  }
+  
+  vizualizarInformacoesGlobais()
+  async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById(‘graficos-container’)
+    container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
+  paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
+
